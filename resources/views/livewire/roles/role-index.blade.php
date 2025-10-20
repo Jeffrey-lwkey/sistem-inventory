4c8b269990e12402f0908cc2a9b844f9a0faac87
Jeffrey-lwkey <jeffreyrosadi17@gmail.com>
2025-10-20 22:25:57 +0700

    Update role-index.blade.php

--- resources/views/livewire/roles/role-index.blade.php
+++ resources/views/livewire/roles/role-index.blade.php
@@ -17,7 +17,7 @@
                 </div>
             </div>
         @endsession
-        @can('role.create')
+        @can('roles.create')
         <a href="{{ route("roles.create") }}"
             class="cursor-pointer px-3 py-2 text-xs font-medium text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300">
             Create Role
@@ -46,19 +46,19 @@
                             @endif
                         </td>
                         <td class="px-6 py-2">
-                            @can('role.view')
+                            @can('roles.view')
                             <a href="{{ route("roles.show", $role->id) }}"
                                 class="mr-2 cursor-pointer px-3 py-2 text-xs font-medium text-white bg-gray-700 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                                 Show
                             </a>
                             @endcan
-                            @can('role.edit')
+                            @can('roles.edit')
                             <a href="{{ route("roles.edit", $role->id) }}"
                                 class="mr-2 cursor-pointer px-3 py-2 text-xs font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                                 Edit
                             </a>
                             @endcan
-                            @can('role.delete')
+                            @can('roles.delete')
                             <button wire:click="delete ({{ $role->id }})" wire:confirm="Do you want to delete this role?"
                                 class="cursor-pointer px-3 py-2 text-xs font-medium text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
                                 Delete
